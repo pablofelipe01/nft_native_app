fcdb489903e926173ec687204bdd5fb42e325e40
Pablo Acebedo <pablofelipe@me.com>
2022-05-24 16:31:53 -0500

    Lot of advance in the front end

--- components/index.js
+++ components/index.js
@@ -4,8 +4,10 @@ import FocusedStatusBar from './FocusedStatusBar'
 import { CircleButton, RectButton } from './Button'
 import DetailsBid from './DetailsBid'
 import DetailsDesc from './DetailsDesc'
+import { SubInfo } from './SubInfo'
 
 export {
     HomeHeader, NFTCard, FocusedStatusBar,
-    CircleButton, RectButton, DetailsBid, DetailsDesc
+    CircleButton, RectButton, DetailsBid, DetailsDesc,
+    SubInfo
 }
--- screens/Details.js
+++ screens/Details.js
@@ -1,8 +1,8 @@
-import React from "react"
-import { View, Text, SafeAreaView, Image, StatusBar, FlatList } from 'react-native'
+import React from "react";
+import { View, Text, SafeAreaView, Image, StatusBar, FlatList } from "react-native";
 
-import { COLORS, SIZES, SHADOWS, FONTS, assets} from '../constants'
-import { CircleButton, RectButton, SubInfo, FocusedStatusBar, DetailsDesc, DetailsBid } from '../components'
+import { COLORS, SIZES, assets, SHADOWS, FONTS } from "../constants";
+import { CircleButton, RectButton, SubInfo, DetailsDesc, DetailsBid, FocusedStatusBar } from "../components";
 
 const DetailsHeader = ({ data, navigation }) => (
   <View style={{ width: "100%", height: 373 }}>
@@ -28,69 +28,64 @@ const DetailsHeader = ({ data, navigation }) => (
 );
 
 const Details = ({ route, navigation }) => {
-  const { data } = route.params
+  const { data } = route.params;
+
   return (
-     <SafeAreaView style={{ flex: 1}} >
-       <FocusedStatusBar 
-        barStyle='dark-content'
-        backgroundColor='transparent'
+    <SafeAreaView style={{ flex: 1 }}>
+      <FocusedStatusBar
+        barStyle="dark-content"
+        backgroundColor="transparent"
         translucent={true}
-       />
+      />
 
-       <View style={{ 
-         width: "100%",
-         position: "absolute",
-         bottom: 0,
-         paddingVertical: SIZES.font,
-         justifyContent: "center",
-         alignItems: "center",
-         backgroundColor: "rgba(255,255,255,0.5)",
-         zIndex: 1,
-       }}>
-         <RectButton 
-          minWidth={170}
-          fontSize={SIZES.large} 
-          {...SHADOWS.dark}
-         />
-       </View>
+      <View
+        style={{
+          width: "100%",
+          position: "absolute",
+          bottom: 0,
+          paddingVertical: SIZES.font,
+          justifyContent: "center",
+          alignItems: "center",
+          backgroundColor: "rgba(255,255,255,0.5)",
+          zIndex: 1,
+        }}
+      >
+        <RectButton minWidth={170} fontSize={SIZES.large} {...SHADOWS.dark} />
+      </View>
 
-       <FlatList 
-          data={data.bids}
-          renderItem={({ item }) => <DetailsBid bid={item} />}
-          keyExtractor={(item) => item.id}
-          showsVerticalScrollIndicator={false}
-          contentContainerStyle={{ paddingBottom: SIZES.extraLarge * 3,}}
-
-          ListHeaderComponent={() => (
-            <React.Fragment>
-              <DetailsHeader data={data} navigation={navigation} />
+      <FlatList
+        data={data.bids}
+        renderItem={({ item }) => <DetailsBid bid={item} />}
+        keyExtractor={(item) => item.id}
+        showsVerticalScrollIndicator={false}
+        contentContainerStyle={{ paddingBottom: SIZES.extraLarge * 3 }}
+        ListHeaderComponent={() => (
+          <React.Fragment>
+              <DetailsHeader data={data} navigation={navigation}/>
               <SubInfo />
               <View style={{ padding: SIZES.font }}>
-                <DetailsDesc data={data} />
-  
-                {data.bids.length > 0 && (
-                  <Text
-                    style={{
-                      fontSize: SIZES.font,
-                      fontFamily: FONTS.semiBold,
-                      color: COLORS.primary,
-                    }}
-                  >
-                    Current Bid
-                  </Text>
-                )}
-              </View>
-            </React.Fragment>
-          )}
+              <DetailsDesc data={data} />
 
+              {data.bids.length > 0 && (
+                <Text
+                  style={{
+                    fontSize: SIZES.font,
+                    fontFamily: FONTS.semiBold,
+                    color: COLORS.primary,
+                  }}
+                >
+                  Current Bid
+                </Text>
+              )}
+            </View>
+              
+          </React.Fragment>
+        )}
+      />
+    </SafeAreaView>
+  );
+};
 
-       
-       
-       />
+export default Details;
 
-     </SafeAreaView>
-      
-  )
-}
-
-export default Details
+            
